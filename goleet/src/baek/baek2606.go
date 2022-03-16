67aed82d2f051f63640046d7be5b3d331f69c068
bexelpatra <83278536+bexelpatra@users.noreply.github.com>
2022-03-16 19:46:16 +0900

    Update baek2606.go
    bfs practise

--- goleet/src/baek/baek2606.go
+++ goleet/src/baek/baek2606.go
@@ -13,10 +13,12 @@ func Baek2606() {
 	var node int
 	var con int
 	var connection [][]int
+	var checked []int
 
 	fmt.Fscanln(r, &node)
 	fmt.Fscanln(r, &con)
 	connection = make([][]int, node+1)
+	checked = make([]int, node+1)
 	for i := 0; i < con; i++ {
 		var a, b int
 		fmt.Fscanln(r, &a, &b)
@@ -26,12 +28,19 @@ func Baek2606() {
 
 	q := make([]int, 0)
 	q = append(q, 1)
-
+	checked[1] = 1
+	total := 0
 	for len(q) > 0 {
 		now := q[0]
 		q = q[1:]
-		q = append(q, connection[now]...)
+		total += 1
+		for _, nodes := range connection[now] {
+			if checked[nodes] == 0 {
+				q = append(q, nodes)
+				checked[nodes] = 1
+			}
+		}
 
 	}
-
+	fmt.Println(total - 1)
 }
