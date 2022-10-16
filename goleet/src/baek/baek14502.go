b93bf4eee8571fb86109758dafb3f37a9aaf1348
a45hvn <bexelpatra@gmail.com>
2022-10-16 23:33:33 +0900

    bfs

--- goleet/src/baek/baek14502.go
+++ goleet/src/baek/baek14502.go
@@ -14,43 +14,58 @@ func Baek14502() {
 	r.Split(bufio.ScanWords)
 	N = scanInt14502(r)
 	K = scanInt14502(r)
-	graphs,virus := scanLine14502(r,N,K)
+	graphs,virus,walls := scanLine14502(r,N,K)
 
-	checked := make([][]bool,N)
-	for v := range checked {
-		checked[v] = make([]bool, K)
-	}
-	
-	fmt.Println()
-	
-	
-	result := make([]int,0)
-	for i := 0; i < N-2; i++ {
-		for j := i+1; j < N-1; j++ {
-			for k := j+1; k < N; k++ {
-				result = append(result, spreadVirus(graphs,checked,virus))
+	max :=0
+	for i := 0; i < len(walls)-2; i++ {
+		wall1 :=walls[i]
+		for j := i+1; j < len(walls)-1; j++ {
+			wall2:=walls[j]
+			for k := j+1; k < len(walls); k++ {
+				wall3:=walls[k]
+
+				checked := make([][]bool,N)
+				for v := range checked {
+					checked[v] = make([]bool, K)
+				}
+				changGraph(&graphs,wall1,wall2,wall3,1)
+				max = func(a,b int)int{
+					if(a>b){
+						return a
+					}
+					return b
+				}(spreadVirus(graphs,checked,virus),max)
+				changGraph(&graphs,wall1,wall2,wall3,0)
 			}
 		}
 	}
-	fmt.Println(result)
+	fmt.Println(max)
 	// for _, v := range graphs {
 	// 	fmt.Println(v)
 	// }
 }
+func changGraph(graph *[][]int, w1,w2,w3 []int,to int){
+	(*graph)[w1[0]][w1[1]] = to
+	(*graph)[w2[0]][w2[1]] = to
+	(*graph)[w3[0]][w3[1]] = to
+}
 
-func scanLine14502(r *bufio.Scanner,n,k int)([][]int,[][2]int){
+func scanLine14502(r *bufio.Scanner,n,k int)([][]int,[][2]int,[][]int){
 	result := make([][]int,n)
 	virus := make([][2]int,0)
+	walls :=make([][]int,0)
 	for i := range result {
 		result[i] = make([]int, k)
 		for j := range result[i] {
 			result[i][j] = scanInt14502(r)
 			if(result[i][j] ==2 ){
 				virus = append(virus, [2]int{i,j})
+			}else if result[i][j] ==0{
+				walls = append(walls, []int{i,j})
 			}
 		}
 	}
-	return result,virus
+	return result,virus,walls
 }
 
 func scanInt14502(r * bufio.Scanner) int{
